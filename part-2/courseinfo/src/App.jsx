10909f305b55a3346702d1c60be74a5a3c1dc706
Vardan <unpredictablebogati@gmail.com>
2025-04-17 11:02:57 +0545

    2.3*: Course information step 8

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -17,7 +17,7 @@ const Content = ({content}) => {
   return(
     <div>
       {content.map(part => <Part {...part} key={part.id}/>)}
-      <p>total of {totalExercises} exercises</p>
+      <b>total of {totalExercises} exercises</b>
     </div>
   )
 }
@@ -44,25 +44,30 @@ const Course = ({course}) => {
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
 
   return <Course course={course} key={course.id}/>;
 }
