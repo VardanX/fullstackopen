cca2e6cb7946200a63ccd062cfb4906616f5733c
Vardan <unpredictablebogati@gmail.com>
2026-03-17 15:49:48 +0545

    3.3: Phonebook backend step 3

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -36,6 +36,16 @@ app.get("/info", (request, response) => {
     response.send(`<h4>${entries}</h4><h4>${now}</h4>`);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const contact = phonebook.find(person => person.id === id)
+    if (contact){
+        response.json(contact)
+    }else{
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
