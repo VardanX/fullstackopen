9995b95ab5efc11efb4f9f00476554ad30ef1763
Vardan <unpredictablebogati@gmail.com>
2026-04-07 13:23:26 +0545

    4.2 Blog List, step 2

---
+++ part-4/bloglist/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const requestLogger = require("./utils/middleware.js");
+
+const app = express();
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use(requestLogger);
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
