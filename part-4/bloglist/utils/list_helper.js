ef2d5e2b96a620bf91846b22b984c1a11ccb1442
Vardan <unpredictablebogati@gmail.com>
2026-04-10 14:26:38 +0545

    4.5*: Helper Functions and Unit Tests, step 3

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -10,6 +10,24 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+    let favorite = blogs[0];
+    if(blogs.length === 0){
+        return {};
+    }
+
+    blogs.forEach((blog) => {
+        if(blog.likes > favorite.likes){
+            favorite = blog
+        }else if (blog.likes === favorite.likes){
+            favorite = blog
+        }
+    })
+    return favorite;
+
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
